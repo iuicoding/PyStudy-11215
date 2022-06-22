b3c7ae74f32206735fbce3729824c471956211ac
Admin <sunminas>
2022-06-22 15:35:04 +0900

    update

---
+++ pystudy_2/이선민.py
@@ -0,0 +1,27 @@
+if score == 100
+    print("1등급")
+elif 90 < score:
+    print("2등급")
+elif 80 < score:
+    print("3등급")
+elif 70 < score:
+    print("4등급")
+elif 60 < score:
+    print("5등급")
+elif 50 < score:
+    print("난 준오쌤한테 주거따")
+
+alist = [10303, 10205, 11215, 10319]
+
+print(alist)
+
+if 11215 in alist
+    print("이선민 출석")
+
+if 10319 not in alist
+    print("김가인 결석")
+
+del alist[10303]
+print(alist)
+
+print(alist + [10205])
